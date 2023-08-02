1fd378ef2707ba881b8bb9f72a58fe7b2af93c3a
faggian <felipesantosfaggian@gmail.com>
2023-08-02 20:35:33 -0300

    app-slap-gnat-dynamic-dimension

---
+++ app2/jogo.js
@@ -0,0 +1,16 @@
+alert('Teste!')
+
+var altura = 0 
+var lagura = 0
+
+function ajustaTamanhoPalcoJogo() {
+	altura = window.innerHeight
+	largura = window.innerWidth
+
+	console.log(altura, largura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+
+
