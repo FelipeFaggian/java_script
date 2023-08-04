58bed7f717c49e240d235d8b6a33652d4189c06e
faggian <felipesantosfaggian@gmail.com>
2023-08-04 19:17:44 -0300

    app-slap-gnat-victory

--- app2/jogo.js
+++ app2/jogo.js
@@ -3,6 +3,7 @@
 var altura = 0 
 var lagura = 0
 var vidas = 1
+var tempo = 15
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -12,6 +13,20 @@ function ajustaTamanhoPalcoJogo() {
 }
 
 ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function() {
+	
+	tempo -= 1
+
+	if (tempo < 0) {
+				clearInterval(cronometro)
+				clearInterval(criaMosquito)
+				alert('Vitória!')
+			} else {
+				document.getElementById('cronometro').innerHTML = tempo
+			}
+	
+	}, 1000)
 
 	function posicaoRandomica() {
 
